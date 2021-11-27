f9b10ee27a7b37a3450bcf0809c8bc0a2e19787b
Milena Gouveia <milenaagouveia@gmail.com>
2021-11-27 17:27:09 +0000

    Adicionei novos setters, getters e documentacao do aviao. Duvida: metodo realizarServico()

--- Tests/Aviao.cpp
+++ Tests/Aviao.cpp
@@ -1,20 +1,26 @@
 #include "Aviao.h"
 using namespace std;
 
-//isabel: tive de implementar o construtor defualt para conseguir fazer build
 Aviao::Aviao() {
     matricula = "";
     capacidade = 0;
     numLugaresReservados = 0;
+    //milena: fiquei na duvida destas tres linhas de codigo; vi no fivheiro da TP5 (game()) e nao sei se é uma boa pratica
+    //ou algo do genero; nao sei se é necessario colocar ou nao
+    planoDeVoo.clear();
+    // esvaziar servicosPorRealizar;
+    //esvaziar servicosRealizados;
+
 }
 
-Aviao::Aviao(string m, unsigned int c, unsigned int nlr,list<Voo> &planVoo, queue <Servico> &sRealizados, queue <Servico> &sPorRealizar ){
+Aviao::Aviao(string m, unsigned int c, unsigned int nlr,list<Voo> &planVoo, queue <Servico> &servRealizados, queue <Servico> &servPorRealizar ){
     this->matricula = m;
     this->capacidade = c;
     this->numLugaresReservados = nlr;
     this->planoDeVoo=planVoo;
-    this->servicosRealizados=sRealizados;
-    this->servicosPorRealizar=sPorRealizar;
+    this->servicosPorRealizar=servPorRealizar;
+    this->servicosRealizados=servRealizados;
+
 }
 
 void Aviao::setMatricula(const string &m) {
@@ -28,6 +34,21 @@ void Aviao::setCapacidade(const unsigned int &c) {
 void Aviao::setNumLugaresReservados(const unsigned int &lugares) {
     this->numLugaresReservados=lugares;
 }
+
+void Aviao::setPlanoDeVoo(const list<Voo> &pVoo) {
+    this->planoDeVoo = pVoo;
+}
+
+void Aviao::setServicosPorRealizar(const queue<Servico> &servPorRealizar) {
+    this->servicosPorRealizar = servPorRealizar;
+}
+
+void Aviao::setServicosRealizados(const queue<Servico> &servRealizados) {
+    this->servicosRealizados = servRealizados;
+}
+
+
+//-------------------------------------------------------------------------------------------------------
 string Aviao::getMatricula() const {
     return this-> matricula;
 }
@@ -40,18 +61,34 @@ unsigned int Aviao::getNumLugaresReservados() const {
     return this->numLugaresReservados;
 }
 
-queue <Servico> Aviao::getServicosRealizados() const{
-    return this->servicosRealizados;
+const list<Voo> &Aviao::getPlanoDeVoo() const {
+    return planoDeVoo;
 }
 
 queue <Servico> Aviao::getServicosPorRealizar() const {
     return this->servicosPorRealizar;
 }
 
+queue <Servico> Aviao::getServicosRealizados() const{
+    return this->servicosRealizados;
+}
+
+
+//TO DO:-------------------------------------------------------------------------
 void Aviao:: addVoo(const Voo &voo){
-
+    this->planoDeVoo.push_back(voo);
 }
 
-void Aviao:: addServico (const Servico &servico){}
-void Aviao::addServicoRealizado (const Servico &srvico){
+void Aviao::addServicoporRealizar(const Servico &servico) {
+    this-> servicosPorRealizar.push(servico);
 }
+
+//milena: nao sei se o codigo esta certo e se devia ser passado algum parametro
+void Aviao::realizarServico (){
+    this->servicosRealizados.push(servicosPorRealizar.front());
+    this->servicosPorRealizar.pop();
+}
+
+
+
+
--- Tests/Aviao.h
+++ Tests/Aviao.h
@@ -10,31 +10,117 @@ using namespace std;
 
 class Aviao{
 private:
+    /**
+     * matricula do avião
+     */
     string matricula;
+
     //isabel: não acaham que os atributos capacidade de numLugaresReservados faz mais sentido ir para a classe voo?
+    /**
+     * capacidade do avião
+     */
     unsigned int capacidade;
+    /**
+     * número de lugares reservados(ocupados) no avião
+     */
     unsigned int numLugaresReservados;
+    /**
+     * todos os voos que o avião tem agendados/por realizar
+     */
     list<Voo> planoDeVoo;
-    queue <Servico> servicosRealizados;
+    /**
+    * todos os serviços aos quais o avião está sujeito
+    */
     queue <Servico> servicosPorRealizar;
+    /**
+    * todos os serviços concluídos
+    */
+    queue <Servico> servicosRealizados;
+
+
 public:
     Aviao();
-    Aviao(string m, unsigned int c, unsigned int nlr,list<Voo> &planVoo, queue <Servico> &sRealizados, queue <Servico> &sPorRealizar );
+    Aviao(string m, unsigned int c, unsigned int nlr,list<Voo> &planVoo, queue <Servico> &servRealizados, queue <Servico> &servPorRealizar);
+    /**
+    * modifica a matrícula do avião
+    * @param m é a matricula do aviao
+    */
     void setMatricula(const string &m);
+    /**
+    * modifica a capacidade do avião
+    * @param c é a capacidade do aviao
+    */
     void setCapacidade(const unsigned int &c);
+    /**
+    * define o  número de lugares reservados(ocupados) no avião
+    * @param lugares é o número de lugares ocupados
+    */
     void setNumLugaresReservados(const unsigned int &lugares);
+    /**
+    * atribui a lista de voos que o avião tem por realizar
+    * @param pVoo é o plano de voos do avião
+    */
+    void setPlanoDeVoo(const list<Voo> &pVoo);
+    /**
+    * define os serviços aos quais o avião está sujeito
+    * @param servPorRealizar são os serviços a realizar
+    */
+    void setServicosPorRealizar(const queue<Servico> &servPorRealizar);
+    /**
+    * define os serviços concluídos
+    * @param servRealizados são os serviços realizados
+    */
+    void setServicosRealizados(const queue<Servico> &servRealizados);
 
+
+    /**
+     *
+     * @return matricula do avião
+     */
     string getMatricula() const;
+    /**
+     *
+     * @return capacidade do avião
+     */
     unsigned int getCapacidade() const;
+    /**
+     *
+     * @return numero de lugares reservados(ocupados) no avião
+     */
     unsigned int getNumLugaresReservados() const;
-    queue <Servico> getServicosRealizados() const;
+    /**
+    *
+    * @return todos os voos que o avião tem agendados/por realizar
+    */
+    const list<Voo> &getPlanoDeVoo() const;
+    /**
+    *
+    * todos os serviços aos quais o avião está sujeito
+    */
     queue <Servico> getServicosPorRealizar() const;
-
+    /**
+    *
+    * @return todos os serviços concluídos
+    */
+    queue <Servico> getServicosRealizados() const;
+    /**
+     *
+     * acrescenta mais um voo aos demais voos que o avião tem como agendados
+     * @param voo é o novo voo que o avião deverá realizar
+     */
     void addVoo(const Voo &voo);
-    void addServico (const Servico &servico);
-    void addServicoRealizado (const Servico &srvico);
+    /**
+     *
+     * acrescenta um novo serviço a realizar
+     * @param servico é o servico a ser adicionado
+     */
+    void addServicoporRealizar (const Servico &servico);
+    /**
+     *
+     * marca um serviço da lista dos serviços agendados (por realizar) como concluído, ou seja, remove-o da lista dos
+     * agendados e acrescenta na dos realizados.
+     */
+    void realizarServico ();
 
-    //isabel: método que adiciona um novo serviço a realizar
-    //isabel: método que passa um serviço da queue dos serviços por realizar para os serviços realizados
 };
 #endif //_AVIAO_H
