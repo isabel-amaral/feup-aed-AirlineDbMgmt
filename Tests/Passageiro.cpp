1154df07667ece921773a16b62adffd99ff66dc1
Isabel Amaral <isabel.andre.amaral@gmail.com>
2021-11-27 11:12:23 +0000

    Documentação e novos metodos para a classe Passageiro adicionados

--- Tests/Passageiro.cpp
+++ Tests/Passageiro.cpp
@@ -1,8 +1,14 @@
 #include "Passageiro.h"
+#include <iostream>
 
-Passageiro::Passageiro(string n, int id) {
+Passageiro::Passageiro(string n, unsigned id, unsigned i, bool menor) {
     this->nome = n;
     this->id = id;
+    this->idade = i;
+    if (idade >= 18)
+        this->menorNaoAcompanhado = false;
+    else
+        this->menorNaoAcompanhado = menor;
 }
 
 string Passageiro::getNome() const {
@@ -11,6 +17,28 @@ string Passageiro::getNome() const {
 
 int Passageiro::getId() const {
     return this->id;
+}
+
+
+unsigned int Passageiro::getIdade() const {
+    return idade;
+}
+
+bool Passageiro::isMenorNaoAcompanhado() const {
+    return menorNaoAcompanhado;
+}
+
+void Passageiro::mostrarBilehtesAdquiridos() const {
+    list<Bilhete>::const_iterator it;
+    for (it = bilhetesAdquiridos.begin(); it != bilhetesAdquiridos.end(); it++) {
+        //necessário alterar depois da classe aeroporto estar associada à classe voo
+        cout << "Bilhete para " << it->getVoo().getDestino() << " com partida de " << it->getVoo().getOrigem() << endl;
+        cout << "Voo número " << it->getVoo().getNum() << endl;
+        //data do voo
+        cout << "Partida presvista às " << it->getVoo().getPartida() << " com chegada prevista às " << it->getVoo().getChegada() << endl;
+        cout << "A duração do voo será aproximadamente " << it->getVoo().getDuracao() << endl;
+        cout << endl;
+    }
 }
 
 void Passageiro::adquirirBilhete(Bilhete& bilhete) {
--- Tests/Passageiro.h
+++ Tests/Passageiro.h
@@ -8,14 +8,59 @@ using namespace std;
 
 class Passageiro {
 private:
+    /**
+     * nome completo do passageiro
+     */
     string nome;
-    int id;
+    /**
+     * número de identificação do passageiro
+     */
+    unsigned int id;
+    /**
+     * idade do passageiro
+     */
+    unsigned int idade;
+    /**
+     * se o passageiro é um menor não acompanhado
+     */
+    bool menorNaoAcompanhado;
+    /**
+     * todos os bilhetes adquiridos pelo passageiro,
+     * cada passageiro pode ter várias viagem planeadas
+     */
     list<Bilhete> bilhetesAdquiridos;
 
 public:
-    Passageiro(string n, int id);
+    Passageiro(string n, unsigned id, unsigned i, bool menor);
+    /**
+     *
+     * @return nome do passageiro
+     */
     string getNome() const;
+    /**
+     *
+     * @return número de identificação do passageiro
+     */
     int getId() const;
+    /**
+     *
+     * @return idade do passageiro
+     */
+    unsigned int getIdade() const;
+    /**
+     *
+     * @return true se o passageiro for menor de idade e estiver a vaigem sem um adulto responsável, falso caso contrário
+     */
+    bool isMenorNaoAcompanhado() const;
+    /**
+     * este método mostra (efetua print) os bilhetes adquiridos pelo passageiro
+     */
+    void mostrarBilehtesAdquiridos() const;
+    /**
+     * este método acrescenta um novo bilhete aos bilhetes adquiridos do passageiro,
+     * não é possível adquirir um bilhete para um voo num data passada
+     * @param bilhete é o bilhete que o passageiro pretende adquirir
+     */
     void adquirirBilhete(Bilhete& bilhete);
 };
 
