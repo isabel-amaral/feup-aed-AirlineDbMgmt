b3e101b91d468079a29575362c21dec109fc11ce
Isabel Amaral <isabel.andre.amaral@gmail.com>
2021-11-27 11:15:17 +0000

    Documentação para a classe Bilhete adicionada

--- Tests/Bilhete.h
+++ Tests/Bilhete.h
@@ -5,11 +5,25 @@
 
 class Bilhete {
 private:
+    /**
+     * voo para o qual o bilhete foi aquirido
+     */
     Voo voo;
+    /**
+     * se o passageiro que adquiriu o bilhete tem direito a levar bagagem de mão
+     */
     bool bagagemDeMao;
 public:
     Bilhete(Voo v, bool bagagem);
+    /**
+     *
+     * @return voo para o qual o bilhete foi adquirido
+     */
     Voo getVoo() const;
+    /**
+     *
+     * @return true se o passageiro tem direito a levar bagagem de mão, falso caso contrário
+     */
     bool temBagagemDeMao() const;
 };
 
