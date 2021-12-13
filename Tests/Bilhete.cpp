3ce51a2c497028194cf4ca34f43be0e441b7484b
Isabel Amaral <isabel.andre.amaral@gmail.com>
2021-12-13 11:51:14 +0000

    Atributo passageiro acrescentado a bilhete

--- Tests/Bilhete.cpp
+++ Tests/Bilhete.cpp
@@ -5,9 +5,10 @@ Bilhete::Bilhete() {
     this->bagagemDeMao = false;
 }
 
-Bilhete::Bilhete(const Voo& v, bool bagagem) {
+Bilhete::Bilhete(const Voo& v, bool bagagem, Passageiro* p) {
     this->voo = v;
     this->bagagemDeMao = bagagem;
+    this->passageiro = p;
 }
 
 const Voo& Bilhete::getVoo() const {
@@ -16,4 +17,16 @@ const Voo& Bilhete::getVoo() const {
 
 bool Bilhete::temBagagemDeMao() const {
     return this->bagagemDeMao;
+}
+
+Passageiro* Bilhete::getPasssageiro() const {
+    return passageiro;
+}
+
+void Bilhete::setBagagemDeMao(bool bagagem) {
+    this->bagagemDeMao = bagagem;
+}
+
+void Bilhete::setPassageiro(Passageiro* p) {
+    this->passageiro = p;
 }
--- Tests/Bilhete.h
+++ Tests/Bilhete.h
@@ -2,6 +2,7 @@
 #define _BILHETE_H
 
 #include "Voo.h"
+#include "Passageiro.h"
 
 class Bilhete {
 private:
@@ -13,9 +14,11 @@ private:
      * se o passageiro que adquiriu o bilhete tem direito a levar bagagem de mão
      */
     bool bagagemDeMao;
+    Passageiro* passageiro;
+
 public:
     Bilhete();
-    Bilhete(const Voo& v, bool bagagem);
+    Bilhete(const Voo& v, bool bagagem, Passageiro* p);
     /**
      *
      * @return voo para o qual o bilhete foi adquirido
@@ -26,6 +29,9 @@ public:
      * @return true se o passageiro tem direito a levar bagagem de mão, falso caso contrário
      */
     bool temBagagemDeMao() const;
+    Passageiro *getPasssageiro() const;
+    void setBagagemDeMao(bool bagagem);
+    void setPassageiro(Passageiro* p);
 };
 
 #endif //_BILHETE_H
