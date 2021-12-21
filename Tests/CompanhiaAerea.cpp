d917554b6109367844ffbc415ffa3e43c8fcd5ec
Isabel Amaral <isabel.andre.amaral@gmail.com>
2021-12-21 01:23:37 -0100

    Teste

--- Tests/CompanhiaAerea.cpp
+++ Tests/CompanhiaAerea.cpp
@@ -32,10 +32,10 @@ vector<Aeroporto>& CompanhiaAerea::getAeroportos() {
     return aeroportos;
 }
 
-void CompanhiaAerea::setVoos(const vector<Voo>& v) {
+/*void CompanhiaAerea::setVoos(const vector<Voo>& v) {
     this->voos = v;
     sort(voos.begin(), voos.end());
-}
+}*/
 
 void CompanhiaAerea::setExcessoPeso(const ExcessoPeso &excessoPeso) {
     this->excessoPeso = excessoPeso;
